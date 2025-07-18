8c3b4f42b90d61187c347ad2d09d627274e259f8
RozerinMacin <rozerinmacin3@gmail.com>
2025-07-18 10:28:31 +0300

    güncelleme

---
+++ Frm_FilmKayit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RozCineWorld
+{
+    public partial class Frm_FilmKayit : Form
+    {
+        public Frm_FilmKayit()
+        {
+            InitializeComponent();
+        }
+    }
+}
