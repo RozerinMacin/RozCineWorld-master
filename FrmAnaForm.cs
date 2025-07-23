e485cfb6384ebd55f4b4066627286dd8c17b87c9
RozerinMacin <160142528+RozerinMacin@users.noreply.github.com>
2025-07-23 12:05:08 +0300

    ana form güncellenmesi

--- FrmAnaForm.cs
+++ FrmAnaForm.cs
@@ -17,50 +17,51 @@ namespace RozCineWorld
             InitializeComponent();
         }
 
-        private void btnkapat_Click(object sender, EventArgs e)
+        private void btnkapat_Click(object sender, EventArgs e)// Kapatma butonuna tıklandığında çalışacak kod bütün programı kapatır.
         {
             Application.Exit();
-
         }
+        // Bu form yüklendiğinde çalışacak kod
         public string kisiAdSoyad = "";
-
         private void FrmAnaForm_Load(object sender, EventArgs e)
         {
+            // Form yüklendiğinde, label2 kontrolüne yetkili kişinin adını ve soyadını atar.
             label2.Text = ("İşlemi Gerçekleştirecek Olan Yetkili Kişi = " + kisiAdSoyad);
         }
 
         private void btnyonetmenkayit_Click(object sender, EventArgs e)
         {
+            // Yönetmen kayıt formunu açar.
             FrmYonetmenkayit frm = new FrmYonetmenkayit();
             frm.ShowDialog();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
+            // Yönetmen listesi formunu açar.
             FrmYonetmenListesi frm = new FrmYonetmenListesi();
             frm.ShowDialog();
         }
-
         private void button1_Click_1(object sender, EventArgs e)
         {
+            // Oyuncu kayıt formunu açar.
             FrmOyuncuKayit frm = new FrmOyuncuKayit();
             frm.ShowDialog();
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
+            // Oyuncu listesi formunu açar.
             FrmOyuncuListesi Frm = new FrmOyuncuListesi();
             Frm.ShowDialog();
         }
-
         private void button3_Click(object sender, EventArgs e)
         {
+            // Salon kayıt formunu açar.
             FrmSalonKayit frm = new FrmSalonKayit();
             frm.ShowDialog();
         }
-
         private void button4_Click(object sender, EventArgs e)
         {
+            // Salon listesi formunu açar.
             Frm_FilmKayit frm = new Frm_FilmKayit();
             frm.ShowDialog();
         }
